c6843cb1aea1b250ab8265d79f1b80142f3538ef
rodrigofigueroa <olivertate274@gmail.com>
2021-01-28 22:55:51 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.sass';
+//TF
+import * as tf from '@tensorflow/tfjs';
 
 function App() {
+  const model = tf.sequential();
+      model.add(tf.layers.dense( {units: 1, inputShape: [1] } ))
+      model.compile( {loss: 'meanSquaredError', optimizer: 'sgd'} )
+      const xs = tf.tensor2d([1,2,3,4],[4,1])
+      const ys = tf.tensor2d([1,3,5,7],[4,1])
+
+      model.fit(xs, ys, {epochs: 10}).then(() =>{
+        model.predict(tf.tensor2d([5],[1,1])).print();
+      })
+  console.log(model);
   return (
     <div className="App">
       <header className="App-header">
